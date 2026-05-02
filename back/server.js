e0210bf8f83b4bf9c97ffe370c0b15cf5e20b10f
shota chikvaidze <shchikvaidze1@gmail.com>
2026-05-02 20:05:23 +0400

    small change

--- back/server.js
+++ back/server.js
@@ -1,3 +1,11 @@
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED REJECTION:', err);
+});
+
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
